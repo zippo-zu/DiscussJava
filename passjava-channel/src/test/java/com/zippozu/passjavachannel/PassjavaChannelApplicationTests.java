ddf447d2728ad95c8aba1b060f6ea4fcfe3b57b7
zippo <zupo@smcaiot.com>
2021-03-16 14:36:06 +0800

    完成java内容社交服务工程初始化

---
+++ passjava-channel/src/test/java/com/zippozu/passjavachannel/PassjavaChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zippozu.passjavachannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaChannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
